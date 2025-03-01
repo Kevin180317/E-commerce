5f2a5dc80af95493736f8203953971cec881590f
N0elung <noelb.davalos@gmail.com>
2025-03-01 13:40:27 -0800

    hero en procreso

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.example.com",
+        port: "",
+        pathname: "/account123/**",
+        search: "",
+      },
+    ],
+  },
+};
--- src/app/pages/hero.tsx
+++ src/app/pages/hero.tsx
@@ -1,31 +1,33 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 function Hero() {
-
-    return (
-        <div className="flex flex-col md:flex-row justify-between items-center p-32 bg-black">
-            <div className="flex-1 m-4 flex md:flex-col">
-                <Image
-                    alt="Hero"
-                    src="/hero.png"
-                    width={750}
-                    height={700}
-                    className='rounded-xl'
-                />
-            </div>
-            <div className="flex-1 m-4">
-                <h1 className="text-4xl font-bold mb-4">¡Bienvenido a mi tienda en línea!</h1>
-                <p className="text-lg mb-4">Encuentra los mejores productos al mejor precio.</p>        
-                <Link href="/products">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Comprar ahora
-                    </button>
-                </Link>
-            </div>
+  return (
+    <section className="h-screen w-screen">
+      <div className="relative">
+        <div className="absolute inset-0">
+          <img
+            src="/bgHero.jpg"
+            alt="background"
+            className="w-auto h-screen "
+          />
         </div>
-    )
+        <div className="absolute h-screen flex flex-col items-center justify-center container mx-auto">
+          <h1 className="text-4xl text-black">
+            Medicia Medical Service and Woocommerce
+          </h1>
+          <p>
+            Medicia as always focused on the best treatments with affordable
+            price through the best medical services
+          </p>
+          <a href="" className="p-4 bg-emerald-400">
+            Shop Now
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
